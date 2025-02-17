75c3fdf0e7164aac4ff139ab6aa8c131f2704692
faizanurahman17 <faizanurahman1711@gmail.com>
2025-02-17 13:59:28 +0530

    added function

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/* 
+                    <details>
+                        <summary>
+                            <button>
+                                <i class="material-symbols-outlined">contacts_product</i>
+                            </button>
+                        </summary>
+                        <ul>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">LinkedIn</a></li>
+                        </ul>
+                    </details>
+*/
+
 const input = document.querySelector('.input-container input');
 const searchIcon = document.querySelector('.input-container i');
 
