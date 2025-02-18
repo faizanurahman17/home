aac1c3446f90a4bfb6894c1110021b9e564977a0
faizanurahman17 <faizanurahman1711@gmail.com>
2025-02-18 15:59:31 +0530

    contact update

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
                         </ul>
                     </details>
 */
-
+const body = document.querySelector('body');
 const input = document.querySelector('.input-container input');
 const searchIcon = document.querySelector('.input-container i');
 
@@ -28,3 +28,53 @@ input.addEventListener('input',()=>{
         searchIcon.classList.remove('searchIconActivated');
     }
 });
+
+const contBtn = document.querySelector('.cont-btn');
+let isTrayOpen = true;
+
+contBtn.addEventListener('click', contactTray);
+
+function contactTray() {
+    isTrayOpen = !isTrayOpen;
+
+    if (isTrayOpen) {
+        let contTray = document.createElement('div');
+        contTray.classList.add('poped-tray');
+        contTray.innerHTML = `
+            <h2>Social Media</h2>
+            <ul>
+                <li><a href="https://wa.me/+917701897049" target="_blank">WhatsApp</a></li>
+                <li><a href="https://instagram.com/faizanurahman17" target="_blank">Instagram</a></li>
+                <li><a href="https://x.com/faizanurahman17" target="_blank">Twitter</a></li>
+                <li><a href="https://facebook.com/faizanurahman17" target="_blank">Facebook</a></li>
+                <li><a href="https://github.com/faizanurahman17" target="_blank">GitHub</a></li>
+                <li><a href="mailto:faizanurahman1711@icloud.com">Email</a></li>
+            </ul>
+        `; 
+        contTray.id = "contTray";
+        document.body.appendChild(contTray);
+    } else {
+        let contTray = document.getElementById("contTray");
+        if (contTray) {
+            contTray.remove();
+        }
+    }
+}
+
+contactTray();
+
+const myHomeBtn = document.querySelector('.my-home');
+
+myHomeBtn.addEventListener('click',myHomeFn);
+
+function myHomeFn(){
+    location.reload();
+}
+
+input.focus();
+
+// if (location.reload()) {
+//     myHomeBtn.style.color = "red";
+// } else {
+//     myHomeBtn.style.color = "black";
+// }
